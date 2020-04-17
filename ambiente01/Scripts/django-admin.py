03ccbe94dfe1a14aaeadf241d7ca1adcd4856432
quejo18 <36921852+quejo18@users.noreply.github.com>
2020-04-17 17:40:24 -0300

    Initial commit

---
+++ ambiente01/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Vinicius\Desktop\Python\projeto01\ambiente01\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
